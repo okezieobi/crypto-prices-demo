3a3771512a76d69ddd9c210cd9f86788e8f5a15a
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-06-09 17:35:26 +0100

    done

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,13 @@ export default class App {
         const url = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${fsyms.join(
             ','
         )}&tsyms=${tsyms.join(',')}&api_key=${env.cryptoAPIKey}`;
-        const data = await axios.get(url);
+        const { data } = await axios.get(url);
         return data;
     }
 }
+
+(async () => {
+    const app = new App();
+    const data = await app.fetchPricePairs(['ETH', 'DASH'], ['USD', 'BTC']);
+    console.log(data);
+})();
